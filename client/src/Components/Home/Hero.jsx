4b05c4df6e207eb559d25eb8df1c0ad7d4a86682
Luckyishim <luckyrajkarnikar@gmail.com>
2025-11-19 18:56:02 +0545

    Added vite to run code

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -6,7 +6,12 @@ const Hero = () => {
             <div>
                 <h5>AI-Powered Resume Reviewer</h5>
             </div>
-            <div></div>
+            <div>
+                <h1>
+                    LAND YOUR DREAM JOB WITH A 
+                    PERFECT RESUME
+                </h1>
+            </div>
             <div></div>
             <div></div>
             <div></div>
